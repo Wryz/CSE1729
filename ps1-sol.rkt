e8b9b7ba3d088a6108473be074df97498c600432
Wrys <wrysplays@gmail.com>
2021-02-23 16:28:00 -0500

    Updated Problem Set 1 to Solutions

--- ps1-sol.rkt
+++ ps1-sol.rkt
@@ -1,50 +1,59 @@
 ;Question 1
 ;Part a
-(* (+ 22 42) (* 54 99))
+(* (+ 22 42)
+   (* 54 99))
 ;Part b
-(* 99 (* 54 (+ 22 42)))
+(* (* (+ 22 42)
+      54)
+   99)
 ;Part c
-(+ (* 64 102) (* 16 (/ 44 22)))
+(+ (* 64 102)
+   (* 16
+      (/ 44 22)))
 
-;Define the limerick variable here
-(define limerick ( + (* 5 11) (/ (+ 12 144 20 (* (sqrt 4) 3)) 7)))
+
+(define limerick (+ (/ (+ 12 144 20 (* 3 (sqrt 4)))
+                       7)
+                    (* 5 11)))
 
 ;Question 3
-;Part a (we are giving you the first one... ;-)
-(define (inc x) (+ x 1) )
-
+;Part a
+(define (inc x) (+ x 1))
 ;Part b
 (define (inc2 x) (inc (inc x)))
-
 ;Part c
-(define (cube x) (expt x 3))
-
+(define (cube x) (* x x x))
 ;Part d
-(define (p x) (expt (+ (expt x 5) (* 16 (expt x 4)) (* 22 (expt x 3)) x 9) 2))
-
+(define (q x)
+        (+ 9 (* x
+                (+ 1 (* x x
+                        (+ 22 (* x
+                                 (+ 16 x))))))))
+(define (p x) (* (q x) (q x)))
 ;Part e
-(define (ninth x) (cube (cube x)))
+(define (ninth x)
+  (cube (cube x))) 
+; Part f
+(define (eighty-first x)
+  (ninth (ninth x)))
 
-;Part f
-(define (eighty-first x) (ninth (ninth x)))
+(define (isbn10-checkdigit x10 x9 x8 x7 x6 x5 x4 x3 x2)
+  (modulo (- 11
+     (modulo (+ (* x10 10)
+                (* x9 9)
+                (* x8 8)
+                (* x7 7)
+                (* x6 6)
+                (* x5 5)
+                (* x4 4)
+                (* x3 3)
+                (* x2 2))
+             11)) 11))
 
-;Replace the ... with the proper function body
-(define (isbn10-checkdigit x10 x9 x8 x7 x6 x5 x4 x3 x2) (modulo(- 11 (modulo (+
-                                                         (* 10 x10)
-                                                         (* 9 x9)
-                                                         (* 8 x8)
-                                                         (* 7 x7)
-                                                         (* 6 x6)
-                                                         (* 5 x5)
-                                                         (* 4 x4)
-                                                         (* 3 x3)
-                                                         (* 2 x2)) 11)) 11))
-
-;Replace the ... with the proper function body
-;Be sure to include your implemenation of isbn10-checkdigit here too
 (define (is-isbn10? x10 x9 x8 x7 x6 x5 x4 x3 x2 x1)
-  (if (= (isbn10-checkdigit x10 x9 x8 x7 x6 x5 x4 x3 x2 x1)
+  (= (isbn10-checkdigit x10 x9 x8 x7 x6 x5 x4 x3 x2)
+     x1))
 
 ;Replace the ... with the proper function body
 (define (fspiral theta)
-  (expt 1.618 (* theta (/ 2 3.142))))
+        (expt 1.618 (* theta (/ 2 3.142))))
