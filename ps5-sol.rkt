a17613a98d428648f54d3c66eb189892e82a2dab
Wrys <wrysplays@gmail.com>
2021-03-17 20:18:27 -0400

    Add files via upload

--- ps5-sol.rkt
+++ ps5-sol.rkt
@@ -11,10 +11,15 @@
   (/ (factorial (* n 2))
      (* (factorial (+ n 1))
         (factorial n))))
-        
-;;DOES NOT WORK (pls help me)
-(define (leibniz-pi n)
-  (- 1 (accumulate + 0 (lambda (x) (/ (expt -1 x) (+ 1 (* x 2)))) 1 (lambda (x) (+ x 1)) n)))
+
+(define (leibniz-pi x)
+  (define (form n)
+    (* (expt (- 1) n) (/ 1(+ 1(* 2 n)))))
+  (define (next n)
+    (+ n 1))
+  (define (add x1 x2)
+    (+ x1 x2))
+  (* 4 (accumulate add 0 form 0 next (- x 1))))
 
 (define (accumulate-tr combiner null-value term a next b)
   (if (= a 0)
@@ -29,8 +34,6 @@
   (if (= n 0)
       1
       (+ 1 (accumulate-tr + 0 (lambda (c) (/ (expt x c) (fact c))) 1 (lambda (c) (+ c 1)) n))))
-
-(e-to-x 10 0)
 
 (define (encode p)
   (let* ((x (car p))
@@ -61,4 +64,21 @@
     (cons (/ (+ (* a c)(* b d)) (+ (expt c 2) (expt d 2)))
           (/ (- (* b c) (* a d)) (+ (expt c 2) (expt d 2))))))
 
-;;HAVE NOT COMPLETED PROBLEM #4
+(define (sum-quadratic-roots a b c)
+  (sub-complex (cons 0 0) (div-complex b a)))
+
+(define (prod-quadratic-roots a b c)
+  (div-complex c a))
+
+(define (sum-cubic-roots a b c d)
+  (define p (cons 0 0))
+  (let ((dived (div-complex b a)))
+    (sub-complex p dived)))
+
+(define (sum-pairs-cubic-roots a b c d)
+  (div-complex c a))
+
+(define (prod-cubic-roots a b c d)
+  (define p (cons 0 0))
+  (let ((dived (div-complex d a)))
+    (sub-complex p dived)))
